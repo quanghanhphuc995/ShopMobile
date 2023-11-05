6dbd643e74c42e6e9d76372bc834decc589db96f
quanghanhphuc995 <quanghanhphuc995@gmail.com>
2023-11-05 20:07:25 +0700

    upload project

---
+++ Shop_Mobile/Shop_Mobile/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Shop_Mobile.Startup))]
+namespace Shop_Mobile
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
