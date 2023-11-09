d9f698ed808403cfa6726133079077d04a591902
quanghanhphuc995 <quanghanhphuc995@gmail.com>
2023-11-09 19:39:20 +0700

    UpLoad view partial dropdow product

---
+++ Shop_Mobile/Shop_Mobile/Models/BUS/NhaSanXuatBUS.cs
@@ -0,0 +1,17 @@
+﻿using ShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop_Mobile.Models.BUS
+{
+    public class NhaSanXuatBUS
+    {
+        public static IEnumerable<NhaSanXuat> DanhSach()
+        {
+            var db = new ShopConnectionDB();
+            return db.Query<NhaSanXuat>("select * from NhaSanXuat where TinhTrang = 0");
+        }
+    }
+}
