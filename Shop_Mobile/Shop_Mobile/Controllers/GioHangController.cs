31671a0ecf570e430a63b7c6d6b5a1147ac7a5ba
quanghanhphuc995 <quanghanhphuc995@gmail.com>
2023-12-15 22:13:51 +0700

    hoàn thành 1/2 phần giỏ hàng

---
+++ Shop_Mobile/Shop_Mobile/Controllers/GioHangController.cs
@@ -0,0 +1,60 @@
+﻿using Shop_Mobile.Models.BUS;
+using ShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop_Mobile.Controllers
+{
+    public class GioHangController : Controller
+    {
+        // GET: GioHang
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        public ActionResult HienThiThongTinSanPham(string maSanPham, GioHang gioHangItem)
+        {
+            var dbGetCart = GioHangBUS.GetCartItem(maSanPham);
+            if(dbGetCart == null)
+            {
+                GioHangBUS.AddOrUpdateCartItem(gioHangItem);
+            }
+            else
+            {
+               
+            }
+            var gioHangDetails = GioHangBUS.GetGioHangDetails();
+            var gioHangViewModels = gioHangDetails.Select(g => new GioHangViewModel
+            {
+                IdGH = g.IdGH,
+                Id = g.Id,
+                MaSanPham = g.MaSanPham,
+                SoLuong = g.SoLuong,
+                TongTien = g.TongTien,
+                MaLoaiSanPham = g.MaLoaiSanPham,
+                MaNhaSanXuat = g.MaNhaSanXuat,
+                TenSanPham = g.TenSanPham,
+                CauHinh = g.CauHinh,
+                HinhChinh = g.HinhChinh,
+                Hinh1 = g.Hinh1,
+                Hinh2 = g.Hinh2,
+                Hinh3 = g.Hinh3,
+                Hinh4 = g.Hinh4,
+                Gia = g.Gia,
+                SoLuongDaBan = g.SoLuongDaBan,
+                LuotView = g.LuotView,
+                TinhTrang = g.TinhTrang,
+                GhiChu = g.GhiChu
+            });
+
+            return View(gioHangViewModels);
+
+
+
+        }
+    }
+}
---
+++ Shop_Mobile/Shop_Mobile/Models/BUS/GioHangBUS.cs
@@ -0,0 +1,107 @@
+﻿using ShopConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop_Mobile.Models.BUS
+{
+    public class GioHangViewModel
+    {
+        public int IdGH { get; set; }
+        public string Id { get; set; }
+        public string MaSanPham { get; set; }
+        public int? SoLuong { get; set; }
+        public int? TongTien { get; set; }
+        public string MaLoaiSanPham { get; set; }
+        public string MaNhaSanXuat { get; set; }
+        public string TenSanPham { get; set; }
+        public string CauHinh { get; set; }
+        public string HinhChinh { get; set; }
+        public string Hinh1 { get; set; }
+        public string Hinh2 { get; set; }
+        public string Hinh3 { get; set; }
+        public string Hinh4 { get; set; }
+        public int? Gia { get; set; }
+        public int? SoLuongDaBan { get; set; }
+        public int? LuotView { get; set; }
+        public string TinhTrang { get; set; }
+        public string GhiChu { get; set; }
+    }
+    public class GioHangBUS
+    {
+        public static GioHang GetCartItem(string maSanPham)
+        {
+            var db = new ShopConnectionDB();
+            return db.SingleOrDefault<GioHang>("SELECT * FROM GioHang WHERE MaSanPham = @0", maSanPham);
+        }
+
+        public static void AddOrUpdateCartItem(GioHang gioHangItem)
+        {
+            var db = new ShopConnectionDB(); 
+
+            var existingCartItem = GetCartItem(gioHangItem.MaSanPham);
+
+            if (existingCartItem != null)
+            {
+                // Nếu sản phẩm đã tồn tại, cập nhật thông tin
+                existingCartItem.SoLuong += gioHangItem.SoLuong;
+                var giaSanPham = db.ExecuteScalar<int>("SELECT Gia FROM SanPham WHERE MaSanPham = @0", existingCartItem.MaSanPham);
+                existingCartItem.TongTien = existingCartItem.SoLuong * giaSanPham;
+
+                db.Update(existingCartItem);
+            }
+            else
+            {
+                // Nếu sản phẩm chưa tồn tại, thêm mới vào giỏ hàng
+                var newProduct = db.FirstOrDefault<SanPham>("select * from SanPham where MaSanPham = @0", gioHangItem.MaSanPham);
+
+                if (newProduct !=null)
+                {
+                    var gioHangItemToAdd = new GioHang
+                    {
+                        MaSanPham = newProduct.MaSanPham,
+                        SoLuong = gioHangItem.SoLuong,
+                        TongTien =newProduct.Gia*gioHangItem.SoLuong
+                    };
+                    db.Insert(gioHangItemToAdd);
+                }
+            }
+        }
+        public static IEnumerable<GioHangViewModel> GetGioHangDetails()
+        {
+            var db = new ShopConnectionDB();
+
+            var sql = @"
+        SELECT
+            GH.IdGH,
+            GH.Id,
+            GH.MaSanPham,
+            GH.SoLuong,
+            GH.TongTien,
+            SP.MaLoaiSanPham,
+            SP.MaNhaSanXuat,
+            SP.TenSanPham,
+            SP.CauHinh,
+            SP.HinhChinh,
+            SP.Hinh1,
+            SP.Hinh2,
+            SP.Hinh3,
+            SP.Hinh4,
+            SP.Gia,
+            SP.SoLuongDaBan,
+            SP.LuotView,
+            SP.TinhTrang,
+            SP.GhiChu
+        FROM
+            GioHang GH
+        JOIN
+            SanPham SP ON GH.MaSanPham = SP.MaSanPham
+    ";
+
+            var result = db.Query<GioHangViewModel>(sql).ToList();
+            // Do something with the result, or return it as needed
+            return result;
+        }
+    }
+}
