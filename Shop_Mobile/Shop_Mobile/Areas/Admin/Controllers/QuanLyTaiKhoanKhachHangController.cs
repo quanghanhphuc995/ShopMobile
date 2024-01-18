d8592d95e664edb91123ef22ee7331d935918892
quanghanhphuc995 <quanghanhphuc995@gmail.com>
2024-01-18 21:31:26 +0700

    thay đổi sql on delete casade, xong phần quản lí tài khoản cho admin

---
+++ Shop_Mobile/Shop_Mobile/Areas/Admin/Controllers/QuanLyTaiKhoanKhachHangController.cs
@@ -0,0 +1,26 @@
+﻿using Shop_Mobile.Models.BUS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop_Mobile.Areas.Admin.Controllers
+{
+    public class QuanLyTaiKhoanKhachHangController : Controller
+    {
+        // GET: Admin/QuanLyTaiKhoanKhachHang
+        public ActionResult Index()
+        {
+            var db = DataBUS.QuanLyTK();
+            return View(db);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string id)
+        {
+            DataBUS.XoaTKKhachHang(id);
+            return Json(new { success = true });
+        }
+    }
+}
---
+++ Shop_Mobile/Shop_Mobile/Models/BUS/DataBUS.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShopConnection;
+
+namespace Shop_Mobile.Models.BUS
+{
+    public class AspNetUserss
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string UserName { get; set; }
+        public int LockoutEnabled { get; set; }
+    }
+    public class DataBUS
+    {
+        public static IEnumerable<AspNetUserss> QuanLyTK()
+        {
+            var db = new ShopConnectionDB();
+            return db.Query<AspNetUserss>("Select * From AspNetUsers Where LockoutEnabled = 1");
+        }
+
+        public static void XoaTKKhachHang(string id)
+        {
+            var db = new ShopConnectionDB();
+            db.Execute("Delete From AspNetUsers Where Id = @0", id);
+        }
+    }
+}
